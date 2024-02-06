c7f60ac7771b13f0daaa124f7642b2cd8d74a50c
Anderson Fernandes <fernandesanderson14@gmail.com>
2024-02-05 22:26:48 -0300

    Move config files to config folder
    Signed-off-by: Anderson Fernandes <fernandesanderson14@gmail.com>

--- config/puma.rb
+++ config/puma.rb
@@ -5,4 +5,4 @@ threads_count = ENV.fetch('PUMA_THREADS') { 5 }.to_i
 threads threads_count, threads_count
 
 port ENV.fetch('PORT') { 5000 }
-workers ENV.fetch('WORKERS') { 1 }.to_i
+workers ENV.fetch('WORKERS') { 0 }.to_i
