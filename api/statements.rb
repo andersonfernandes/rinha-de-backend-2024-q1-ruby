6c07c9076cc614ab9c9fbc489721599465cec8a4
Anderson Fernandes <fernandesanderson14@gmail.com>
2024-02-06 12:06:17 -0300

    Add extrato initial implementation
    Signed-off-by: Anderson Fernandes <fernandesanderson14@gmail.com>

--- api/statements.rb
+++ api/statements.rb
@@ -1,8 +1,28 @@
+require './models/client'
+require './models/transaction'
+
 module Api
   module Statements
     def self.registered app
       app.get '/clientes/:id/extrato' do
-        'extrato'
+        client = Models::Client[params[:id]]
+        transactions = Models::Transaction.where(client_id: params[:id])
+
+        {
+          saldo: {
+            total: 0, # TODO: implement calculation
+            data_extrato: Time.now.to_s,
+            limite: client[:limit]
+          },
+          ultimas_transacoes: transactions.map do |transaction|
+            {
+              valor: transaction.value,
+              tipo: transaction.type,
+              descricao: transaction.description,
+              realizada_em: transaction.at
+            }
+          end
+        }.to_json
       end
     end
   end
--- app.rb
+++ app.rb
@@ -2,5 +2,9 @@ require 'sinatra'
 require './api/root'
 
 class Application < Sinatra::Base
+  before do
+    content_type :json
+  end
+
   register Api::Root
 end
