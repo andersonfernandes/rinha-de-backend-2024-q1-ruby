e0c02dee8dd02d0865146348e4889cfcfbc9b398
Anderson Fernandes <fernandesanderson14@gmail.com>
2024-02-09 22:36:04 -0300

    Update sequel pool params
    Signed-off-by: Anderson Fernandes <fernandesanderson14@gmail.com>

--- config/database.rb
+++ config/database.rb
@@ -3,7 +3,11 @@ require "logger"
 
 module Database
   def self.init
-    @connection ||= Sequel.connect(ENV["DATABASE_URL"], logger: Logger.new(STDOUT))
+    @connection ||= Sequel.connect(
+      ENV["DATABASE_URL"],
+      logger: Logger.new(STDOUT),
+      pool_timeout: 10,
+    )
     @connection.sql_log_level = :debug
     Sequel::Model.db = @connection
     Sequel::Model.plugin :json_serializer
